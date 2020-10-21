b9023fa2b3c3eb3f68a80cded74c1a93327b85b0
i_m.Divyansh <Divyanshbasera36@gmail.com>
2020-10-21 12:18:39 +0530

    Updates

---
+++ precache-manifest.34d40987e6331bf895f4eabb2857afd0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11348d41f7194b4582ab8cbf303ae824",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "75fce025461c030a821a",
+    "url": "/netflix/static/css/main.8199fe15.chunk.css"
+  },
+  {
+    "revision": "74eb223b7fa3f5812779",
+    "url": "/netflix/static/js/2.8f8502c6.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/netflix/static/js/2.8f8502c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fce025461c030a821a",
+    "url": "/netflix/static/js/main.c562f27f.chunk.js"
+  },
+  {
+    "revision": "2dddb01aa0a4e9e8f0b8",
+    "url": "/netflix/static/js/runtime-main.5625af0b.js"
+  },
+  {
+    "revision": "7f70832e789ba928f071082d052b2e98",
+    "url": "/netflix/static/media/BB.7f70832e.jpg"
+  },
+  {
+    "revision": "d47319d82494cf5737fd116e084bd785",
+    "url": "/netflix/static/media/BH.d47319d8.jpg"
+  },
+  {
+    "revision": "d2f07e5323cc5ed7ae0a499181a42bda",
+    "url": "/netflix/static/media/Dark1.d2f07e53.jpg"
+  },
+  {
+    "revision": "2a220ec639724a9f9fe485e319c32ae6",
+    "url": "/netflix/static/media/LK.2a220ec6.jpg"
+  },
+  {
+    "revision": "764f3c1a6f6957fbd122f08a17b6fdde",
+    "url": "/netflix/static/media/ST.764f3c1a.jpg"
+  },
+  {
+    "revision": "da5033f49e1b443289b9a66556b0ffbb",
+    "url": "/netflix/static/media/TW.da5033f4.jpg"
+  }
+]);
